717fa5f061e5b6f10141219f8469f41aca9ba512
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-16 22:54:55 +0100

    small changes to script

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,9 +13,27 @@ patterns = {
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: chr) -> str:
-    '''
-    Filters log lines
-    '''
+                 separator: str) -> str:
+    """
+    Filters a log message by redacting specified fields.
+
+    This function replaces the values of fields in a log message with a provided
+    redaction string. It uses regular expressions to match field names and their
+    values based on a separator character.
+
+    Args:
+      fields: A list of field names to be redacted from the message.
+      redaction: The string to be used for replacing the matched field values.
+      message: The log message string to be filtered.
+      separator: The character used as a separator between fields and their values
+                 in the log message.
+
+    Returns:
+      A new string with the specified fields redacted using the provided redaction string.
+    """
     return re.sub(patterns['to_replace'](fields, separator),
                   patterns['replacement'](redaction), message)
+
+
+if __name__ == "__main__":
+    main()
