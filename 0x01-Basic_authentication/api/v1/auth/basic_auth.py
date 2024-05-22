a2b475c19cf00ab354919296f13e270bc5798aa5
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-22 03:18:39 +0100

    task 11

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -97,3 +97,12 @@ class BasicAuth(Auth):
             return users[0]  # Return User if password is valid
 
         return None  # Return None if password is invalid
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the current user from a request.
+        """
+        auth_header = self.authorization_header(request)
+        enc_token = self.extract_base64_authorization_header(auth_header)
+        auth_token = self.decode_base64_authorization_header(enc_token_token)
+        email, password = self.extract_user_credentials(auth_token)
+        return self.user_object_from_credentials(email, password)
