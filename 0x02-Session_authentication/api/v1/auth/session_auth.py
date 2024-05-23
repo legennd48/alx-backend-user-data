73c731c13d15c37a787be3622a9f0e86b7a1138b
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-23 22:06:34 +0100

    completed task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Module for session authentication
+'''
+from .auth import Auth
+from flask import session
+
+
+class SessionAuth(Auth):
+    '''
+    Temporarily empty
+    '''
