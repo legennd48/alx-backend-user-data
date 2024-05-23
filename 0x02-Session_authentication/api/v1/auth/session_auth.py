155a3e74c5cf82b2df19c7bde8b13f6a3662440a
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-23 22:37:43 +0100

    completed task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,9 +4,21 @@ Module for session authentication
 '''
 from .auth import Auth
 from flask import session
+import uuid
 
 
 class SessionAuth(Auth):
     '''
     Temporarily empty
     '''
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''
+        Creates a session id for a given user id
+        '''
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id = {session_id: user_id}
+        return session_id
