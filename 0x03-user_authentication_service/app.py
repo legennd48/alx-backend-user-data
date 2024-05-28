8c08e7c26041b2feb39717775f04b3e73f18857f
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-29 00:11:49 +0100

    initial commit, task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 '''
 Basic flask app
 '''
-from flask import Flask, jsonify, request
+from flask import abort, Flask, jsonify, request
 from auth import Auth
 from user import User
 
@@ -31,6 +31,23 @@ def users() -> str:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": user.email, "message": "user created"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    '''
+    handles login, by taking email and pw, from form data.
+    verifies the informatin and sets a cookie with the session id
+    '''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
