b453ab3313c62deebff9dacd69ebeab1b0d18a9a
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-28 14:35:14 +0100

    turned of echo and fixed imports

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -3,7 +3,8 @@
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from typing import Dict
@@ -19,7 +20,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
