9528581ee35a9e559da54646e29c9884b084e323
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-28 17:45:52 +0100

    initial commit after implementing routh: /users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,9 +2,12 @@
 '''
 Basic flask app
 '''
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from user import User
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/')
@@ -15,5 +18,20 @@ def basic() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    '''
+    method: POST /users
+    handles user registeration
+    '''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": user.email, "message": "user created"})
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
