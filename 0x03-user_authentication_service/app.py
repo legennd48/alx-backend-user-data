17c45d723e3b4e6603bd8494c2e79ba4158135bb
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-29 18:15:42 +0100

    typo fix

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -93,7 +93,7 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": token})
 
 
-@app.route('/reset_password', methods=['PUT'], strict_slashed=False)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
     '''
     handles password reset/update
