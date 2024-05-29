a3c839a385c61dff34dd27db477ad2ab980fefa0
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-29 14:49:10 +0100

    initial commit for reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -89,3 +89,15 @@ class Auth:
         '''
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        '''
+        generates password reset token
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_token = str(uuid.uuid4())
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
