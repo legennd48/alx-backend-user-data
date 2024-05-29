377e3ce852ab3ea6d6744a479c029b72c5d5d5e1
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-29 15:09:39 +0100

    initial ccommit of task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -79,5 +79,19 @@ def profile() -> str:
     abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    '''
+    retieves password reset token of a user.
+    email is retried from Form
+    '''
+    email = request.form.get('email')
+    try:
+        token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": token})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
