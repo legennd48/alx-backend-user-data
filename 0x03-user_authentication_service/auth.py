95bc7a15915b27bf76f1af148f9ea46ffbce4149
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-05-29 18:00:38 +0100

    initial commit for task 18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -101,3 +101,17 @@ class Auth:
         reset_token = str(uuid.uuid4())
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        '''
+        handles password reset buy getting the reset token,
+        locate the user and update the new password
+        '''
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        new_password = _hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=new_password, reset_token=None)
+        return None
